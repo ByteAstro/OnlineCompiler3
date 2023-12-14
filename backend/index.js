71cbe157ac704e3d1da2483d23042e06946acf04
Byte Astro <biswajitswain348@gmail.com>
2023-12-14 09:07:50 +0530

    mongoDB conn

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 const app = express();
+dotenv.config();
 const { generateFile } = require('./generateFile');
 const { executeCpp } = require('./executeCpp');
 const { executePy } = require('./executePy');
 
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
